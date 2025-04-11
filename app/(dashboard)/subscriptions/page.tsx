41ed70ea7f5b2d02363b3581515213c28c74b1ad
Cedrick Cantero <cedys02@gmail.com>
2025-04-12 02:35:55 +0800

    Subscriptions

--- app/(dashboard)/subscriptions/page.tsx
+++ app/(dashboard)/subscriptions/page.tsx
@@ -64,7 +64,7 @@ export default function ExpensesPage() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-center justify-between">
-        <h1 className="text-2xl font-bold">Expenses</h1>
+        <h1 className="text-2xl font-bold">Subscriptions</h1>
       </div>
       <Tabs defaultValue="all" className="w-full">
         <TabsList>
