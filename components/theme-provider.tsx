b254f75a79180c9f0dddef3d077176d4198fd6b6
Cedrick Cantero <cedys02@gmail.com>
2025-04-18 13:48:46 +0800

    Fix the root page and theme probvider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,8 +4,31 @@ import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
+  useTheme
 } from 'next-themes'
+import { useAuth } from '@/lib/auth-context'
+import { UserSettings } from '@/types/userSettings'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider {...props}>
+      <ThemeSync>{children}</ThemeSync>
+    </NextThemesProvider>
+  )
+}
+
+function ThemeSync({ children }: { children: React.ReactNode }) {
+  const { setTheme } = useTheme()
+  const { userSettings } = useAuth()
+  
+  React.useEffect(() => {
+    if (userSettings) {
+      const settings = userSettings as unknown as UserSettings
+      if (settings.preferences?.theme) {
+        setTheme(settings.preferences.theme)
+      }
+    }
+  }, [userSettings, setTheme])
+  
+  return <>{children}</>
 }
